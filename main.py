29e4cee6c0b25ee15fe6f682af130d4a1a22290f
jmatuzak01 <matuzakjosiah@gmail.com>
2026-02-25 22:36:40 -0500

    Added API key check + hardcoded response

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+if not os.environ.get("GEMINI_API_KEY"):
+    raise ValueError("GEMINI_API_KEY environment variable is not set.")
+else:
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent!")
-
-
+    reponse = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(reponse.text)
 if __name__ == "__main__":
     main()
